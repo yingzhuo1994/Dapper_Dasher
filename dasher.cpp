af0f2f3fcbe533b8c7dc097fb53d17c4317c28c2
yingzhuo <yingzhuo1994@gmail.com>
2021-09-25 19:16:42 -0500

    gravity and acceleration

---
+++ dasher.cpp
@@ -0,0 +1,54 @@
+#include "raylib.h"
+int main()
+{
+    // window dimensions
+    const int windowWidth = 512;
+    const int windowHeight = 380;
+    // initialize the window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
+
+    // acceleration due to gravity (pixels/frame)/frame
+    const int gravity{1};
+
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // perform ground check
+        if (posY >= windowHeight - height)
+        {
+            // rectangle is on the granoud
+            velocity = 0;
+        }
+        else
+        {
+            // rectangle is in the air
+            // apply gravity
+            velocity += gravity;
+        }
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+
+        // update position
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+        // stop drawing
+        EndDrawing();
+    }
+    CloseWindow();
+}
