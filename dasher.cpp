bbc3be292cd79dd207271b5cb95642ff4e08a19b
yingzhuo <yingzhuo1994@gmail.com>
2021-09-28 17:42:40 -0500

    Update Animation Data using function

--- dasher.cpp
+++ dasher.cpp
@@ -9,7 +9,28 @@ struct AnimData
     float runningTime;
 };
 
+bool isOnGround(AnimData data, int windowHeight)
+{
+    return data.pos.y >= windowHeight - data.rec.height;
+}
 
+AnimData updateAnimData(AnimData data, float deltaTime, int maxFrame)
+{
+    // update running time
+    data.runningTime += deltaTime;
+    if (data.runningTime >= data.updateTime)
+    {
+        data.runningTime = 0.0;
+        // update animation frame
+        data.rec.x = data.frame * data.rec.width;
+        data.frame++;
+        if (data.frame > maxFrame)
+        {
+            data.frame = 0;
+        }
+    }
+    return data;
+}
 
 int main()
 {
@@ -82,7 +103,7 @@ int main()
         ClearBackground(WHITE);
 
         // perform ground check
-        if (scarfyData.pos.y >= windowDimensions[1] - scarfyData.rec.height)
+        if (isOnGround(scarfyData, windowDimensions[1]))
         {
             // rectangle is on the granoud
             velocity = 0;
@@ -112,36 +133,12 @@ int main()
         // update scarfy's animation frame
         if (!isInAir)
         {
-            // update running time
-            scarfyData.runningTime += dT;
-            if (scarfyData.runningTime >= scarfyData.updateTime)
-            {
-                scarfyData.runningTime = 0.0;
-                // update animation frame
-                scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;
-                scarfyData.frame++;
-                if (scarfyData.frame > 5)
-                {
-                    scarfyData.frame = 0;
-                }
-            }
+            scarfyData = updateAnimData(scarfyData, dT, 5);
         }
 
         for (int i = 0; i < sizeOfNebulae; i++)
         {
-            // update nebula animation frame
-            nebulae[i].runningTime += dT;
-            if (nebulae[i].runningTime >= nebulae[i].updateTime)
-            {
-                nebulae[i].runningTime = 0.0;
-                // update animation frame
-                nebulae[i].rec.x = nebulae[i].frame * nebulae[i].rec.width;
-                nebulae[i].frame++;
-                if (nebulae[i].frame > 7)
-                {
-                    nebulae[i].frame = 0;
-                }
-            } 
+            nebulae[i] = updateAnimData(nebulae[i], dT, 7);
         }
 
         for (int i = 0; i < sizeOfNebulae; i++)
